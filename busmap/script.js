1de3c69477dcb72ace117977544fb333ca31d41b
btzy <btzy1996@hotmail.com>
2014-03-14 17:56:52 +0800

    jquerysvg and more

--- busmap/script.js
+++ busmap/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    $("#faderbox").css("opacity","0.5");
+	var data_zoom=1;
+	var data_angle=1;
+	var edit_lock=true;
+	$("#faderbox").css("opacity","0.5");
 	$("#newbutton").click(function() {
         $("#faderbox").css("display","block");
 		$("#newbox").css("display","block");
@@ -44,5 +47,33 @@ $(document).ready(function() {
 		}
     });
 	function createnewsvg(width,height,anglestep){
+		$("#svgmover").css("width",width+'px');
+		$("#svgmover").css("height",height+'px');
+		//$("#svgmover").html('<svg width="'+width+'" height="'+height+'"></svg>');
+		//$(SVG("svg").attr("width",width).attr("height",height)).appendTo($("#svgmover"));
+		//svg=$(SVG("svg"));
+		//svg.data("angle",anglestep);
+		$("#svgmover").svg({"width":width,"height":height});
+		//$("#svgmover").svg("get").attr("data-angle",anglestep);
+		data_angle=anglestep;
+		data_zoom=Math.min($("#svgbox").width()/width,$("#svgbox").height()/height);
+		$("svg").attr("data-angle",data_angle);
+		$("svg").attr("data-zoom",data_zoom);
+		scalesvgmover();
+		translatesvgmovertocenter();
+		setTimeout(function(){edit_lock=false;},500);
+	}
+	$("#svgmover").mousemove(function(e) {
+        var x=e.pageX-$("#svgmover").offset().left;
+		var y=e.pageY-$("#svgmover").offset().top;
+    });
+	function SVG(tag){
+		return document.createElementNS('http://www.w3.org/2000/svg', tag);
+	}
+	function scalesvgmover(){
+		$("#svgmover").css({transform:"scale("+data_zoom+","+data_zoom+")","-webkit-transform":"scale("+data_zoom+","+data_zoom+")","-moz-transform":"scale("+data_zoom+","+data_zoom+")","-o-transform":"scale("+data_zoom+","+data_zoom+")","-ms-transform":"scale("+data_zoom+","+data_zoom+")"});
+	}
+	function translatesvgmovertocenter(){
+		//$("#svgmover").
 	}
 });
