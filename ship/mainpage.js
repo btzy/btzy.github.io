95a6f83597133671f2b341f50b746cf1ef5d1e0e
Bernard Teo <btzy1996@hotmail.com>
2014-07-02 22:59:16 +0800

    t

--- ship/mainpage.js
+++ ship/mainpage.js
@@ -1,11 +1,10 @@
 // JavaScript Document
 $(document).ready(function(e) {
-    $(document.body).on("touchmove MSPointerMove", function(e) {
+    $(document).on("touchmove MSPointerMove", function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 	});
 	$("#playtitlebutton").click(function(e) {
-        alert("H");
 		$("#titlebuttonbox").fadeOut(300,"linear",function(){
 			$("#settingbuttonbox").fadeIn(300,"linear");
 		});
