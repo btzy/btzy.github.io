1273ae805ff02ddc02103e1cda937b00dbc94d80
Bernard Teo <btzy1996@hotmail.com>
2014-07-02 22:56:20 +0800

    t

--- ship/mainpage.js
+++ ship/mainpage.js
@@ -1,7 +1,8 @@
 // JavaScript Document
 $(document).ready(function(e) {
-    $(document).on("touchmove MSPointerMove", function(e) {
+    $(document.body).on("touchmove MSPointerMove", function(e) {
 		e.preventDefault();
+		e.stopPropagation();
 	});
 	$("#playtitlebutton").click(function(e) {
         alert("H");
