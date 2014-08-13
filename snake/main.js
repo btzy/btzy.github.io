078ac356179e1fc2c27eaff8c695342625cdbc2a
Bernard Teo <btzy1996@hotmail.com>
2014-08-13 16:15:26 +0800

    t

--- snake/main.js
+++ snake/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(e){
 	// Start managers:
 	board_manager.Start(document.getElementById("mainsvg"));
 	/*snake_manager.Start({Grid:["01110","11011","11111"],Snake:[new Point(1,2),new Point(1,1)]});*/
-	snake_manager.Start({Grid:["01110","11011","11111"],Snake:[new Point(0,2),new Point(1,2),new Point(1,1)]});
+	snake_manager.Start({Grid:["01110","11111","11011","11111","01110"],Snake:[new Point(0,2),new Point(1,2),new Point(1,1)]});
 	input_manager.Start(false);
 	//snake_manager.Start(new Point(5,3),[[0,1,1,1,0],[1,1,0,1,1],[1,1,1,1,1]]);
 	
